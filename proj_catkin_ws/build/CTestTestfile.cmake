3f5aa3f90e81250fba41ed5f7542ff9764f448a2
Artur46s <160283416+Artur46s@users.noreply.github.com>
2024-05-16 00:01:12 +0100

    added folder with catkin workspace
    the python code is under src/base_pkg/scripts

---
+++ proj_catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,11 @@
+# CMake generated Testfile for 
+# Source directory: /home/artur-ubunto/Desktop/proj_ws/src
+# Build directory: /home/artur-ubunto/Desktop/proj_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("freenect_stack/freenect_launch")
+subdirs("freenect_stack/freenect_stack")
+subdirs("base_pkg")
+subdirs("freenect_stack/freenect_camera")
---
+++ proj_catkin_ws/build/base_pkg/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/artur-ubunto/Desktop/proj_ws/src/base_pkg
+# Build directory: /home/artur-ubunto/Desktop/proj_ws/build/base_pkg
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
